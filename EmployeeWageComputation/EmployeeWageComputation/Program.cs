a46822cc4451dd54696f04273e40edfb23b6064d
Soumya <soumyadipta0077@gmail.com>
2020-09-29 12:38:02 +0530

    [Soumya] Add: Adding Employee Attendance

---
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+            const int isPresent = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == isPresent)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
