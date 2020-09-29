395cedeb364877631dc3d20dcaefb471d180f0d2
Soumya <soumyadipta0077@gmail.com>
2020-09-29 13:08:58 +0530

    [Soumya] Refactor: Using Switch Case

--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -6,25 +6,41 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            const int isPresent = 1;
-            const int empRatePerHour = 20;
-            const int FULLTIME = 1;
-            int empHours;
             int empWage;
+            const int empRatePerHour = 20;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
-            if (empCheck == isPresent)
-            {
-                int isFullOrPartTime = random.Next(0, 2);
-                if (isFullOrPartTime == FULLTIME)
-                    empHours = 8;
-                else
-                    empHours = 4;
-            }
-            else
-                empHours = 0;
+            int empHours = getEmpHours(empCheck);
             empWage = empHours * empRatePerHour;
-            Console.WriteLine("Employee Wage: " + empWage);
+            Console.WriteLine("Employee Wage : " + empWage);
+        }
+        public static int getEmpHours(int empCheck)
+        {
+            int empHours = 0;
+            const int FULLTIME = 1;
+            const int PARTTIME = 0;
+            const int PRESENT = 1;
+            const int ABSENT = 0;
+            Random random = new Random();
+            switch (empCheck)
+            {
+                case PRESENT:
+                    int isFullOrPartTime = random.Next(0, 2);
+                    switch (isFullOrPartTime)
+                    {
+                        case FULLTIME:
+                            empHours = 8;
+                            break;
+                        case PARTTIME:
+                            empHours = 4;
+                            break;
+                    }
+                    break;
+                case ABSENT:
+                    empHours = 0;
+                    break;
+            }
+            return empHours;
         }
     }
 }
