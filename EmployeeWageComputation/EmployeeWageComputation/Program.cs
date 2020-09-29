500fc0070ee1ec6ac90b1fe99137185ea48df57b
Soumya <soumyadipta0077@gmail.com>
2020-09-29 13:50:05 +0530

    [Soumya] Refactor: Calculating Employee Wage Using Class Methods

--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -4,20 +4,20 @@ namespace EmployeeWageComputation
 {
     class Program
     {
-        static void Main(string[] args)
+        const int EMP_RATE_PER_HOUR = 20;
+        const int TOTAL_HOURS = 100;
+        const int TOTAL_DAYS = 20;
+        const int FULLTIME = 1;
+        const int PARTTIME = 2;
+        const int ABSENT = 0;
+        public static void computeEmpWage()
         {
-            int isFullOrPartTime;
-            int empHours = 0;
-            const int EMP_RATE_PER_HOUR = 20;
-            const int TOTAL_HOURS = 100;
-            const int TOTAL_DAYS = 20;
-            const int FULLTIME = 1;
-            const int PARTTIME = 2;
-            const int ABSENT = 0;
             int countOfHours = 0;
             int countOfDays = 0;
             int totalWage = 0;
             Random random = new Random();
+            int isFullOrPartTime;
+            int empHours = 0;
             while (countOfHours <= TOTAL_HOURS && countOfDays < TOTAL_DAYS)
             {
                 isFullOrPartTime = random.Next(0, 3);
@@ -38,6 +38,10 @@ namespace EmployeeWageComputation
                 countOfDays += 1;
             }
             Console.WriteLine("Employee total Wage: " + totalWage);
+        }
+        static void Main(string[] args)
+        {
+            computeEmpWage();            
         }
     }
 }
