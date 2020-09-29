315f211b736104384133f718e26c53cc39005679
Soumya <soumyadipta0077@gmail.com>
2020-09-29 14:22:44 +0530

    [Soumya] Refactor: Calculate Total Wage For Each Company Using Different Classes

--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -7,39 +7,14 @@ namespace EmployeeWageComputation
         const int FULLTIME = 1;
         const int PARTTIME = 2;
         const int ABSENT = 0;
-        public static void computeEmpWage(string company,int EMP_RATE_PER_HOUR,int totalHours, int totalDays)
-        {
-            int countOfHours = 0;
-            int countOfDays = 0;
-            int totalWage = 0;
-            Random random = new Random();
-            int isFullOrPartTime;
-            int empHours = 0;
-            while (countOfHours <= totalHours && countOfDays < totalDays)
-            {
-                isFullOrPartTime = random.Next(0, 3);
-                switch (isFullOrPartTime)
-                {
-                    case FULLTIME:
-                        empHours = 8;
-                        break;
-                    case PARTTIME:
-                        empHours = 4;
-                        break;
-                    case ABSENT:
-                        empHours = 0;
-                        break;
-                }
-                totalWage = totalWage + (empHours * EMP_RATE_PER_HOUR);
-                countOfHours += empHours;
-                countOfDays += 1;
-            }
-            Console.WriteLine("Total Employee Wage for company " +company+" :"+ totalWage);
-        }
         static void Main(string[] args)
         {
-            computeEmpWage("Dmart",20,100,20);
-            computeEmpWage("Reliance", 20, 200, 30);
+            EmpWageBuilderObject dmart = new EmpWageBuilderObject("Dmart",20,100,20);
+            EmpWageBuilderObject Reliance = new EmpWageBuilderObject("Reliance", 20, 200, 30);
+            dmart.computeEmpWage();
+            dmart.toString();
+            Reliance.computeEmpWage();
+            Reliance.toString();
         }
     }
 }
