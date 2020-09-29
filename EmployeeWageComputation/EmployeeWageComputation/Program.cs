3420c542fe650cdac01f877427b93dad85968b56
Soumya <soumyadipta0077@gmail.com>
2020-09-29 13:00:19 +0530

    [Soumya] Refactor: Calculate Full Time Or Part Time Wage

--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -8,20 +8,21 @@ namespace EmployeeWageComputation
         {
             const int isPresent = 1;
             const int empRatePerHour = 20;
+            const int FULLTIME = 1;
             int empHours;
             int empWage;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == isPresent)
             {
-                Console.WriteLine("Employee is Present");
-                empHours = 8;
+                int isFullOrPartTime = random.Next(0, 2);
+                if (isFullOrPartTime == FULLTIME)
+                    empHours = 8;
+                else
+                    empHours = 4;
             }
             else
-            {
-                Console.WriteLine("Employee is Absent");
                 empHours = 0;
-            }
             empWage = empHours * empRatePerHour;
             Console.WriteLine("Employee Wage: " + empWage);
         }
